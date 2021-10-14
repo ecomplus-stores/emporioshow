2be3a18f773e87f8e1a06b07463a4f81efddbb78
Leonardo Matos <leomp120894@gmail.com>
2021-10-14 17:56:31 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -7,7 +7,7 @@ module.exports = () => ({
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html'),
       './js/ProductGallery.js': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.js'),
       './scss/ProductGallery.scss': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.scss'),
-      './base-config/': path.resolve(__dirname, 'template/js/netlify-cms/base-config.js')
+      './base-config/index.js': path.resolve(__dirname, 'template/js/netlify-cms/base-config.js')
     }
   }
 })
