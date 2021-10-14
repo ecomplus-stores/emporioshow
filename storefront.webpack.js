338bacd2332c1a47bc5e2fb2c380367a74dd3392
eduvlemes <edu.vlemes@gmail.com>
2021-10-13 22:58:13 -0300

    recipes

---
+++ storefront.webpack.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = () => ({
+  resolve: {
+    alias: {
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html'),
+      './js/ProductGallery.js': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.js'),
+      './scss/ProductGallery.scss': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.scss')
+    }
+  }
+})
