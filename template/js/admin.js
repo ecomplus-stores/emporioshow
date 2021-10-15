296fbe22212fb22df9e1915adf1a0d29367ca2f9
Leonardo Matos <leomp120894@gmail.com>
2021-10-15 13:49:20 -0300

    Update admin.js

--- template/js/admin.js
+++ template/js/admin.js
@@ -2,6 +2,9 @@ import '@ecomplus/storefront-template/template/js/lib/config'
 import EcomRouter from '@ecomplus/storefront-router'
 import initNetlifyCms from '@ecomplus/storefront-template/template/js/netlify-cms/init'
 
+// https://developers.e-com.plus/client/ecomClient.html#.store
+import { store } from '@ecomplus/client'
+
 document.title = `Admin ~ ${document.title}`
 
 const state = {}
@@ -16,13 +19,24 @@ new EcomRouter().list()
   })
 
   .finally(() => {
-    if (window.PKG_BASE_DIR === undefined) {
-      window.PKG_BASE_DIR = ''
-    }
-    initNetlifyCms(window.CMS_CUSTOM_CONFIG, {
-      baseDir: window.PKG_BASE_DIR,
-      state
-    }).then(config => {
-      console.log('Netlify CMS config:', config)
-    })
+    store({ url: '/grids' })
+      .then(({ data }) => {
+        state.grids = data.result
+      })
+      .catch(err => {
+        console.error(err)
+        state.grids = []
+      })
+
+      .finally(() => {
+        if (window.PKG_BASE_DIR === undefined) {
+          window.PKG_BASE_DIR = ''
+        }
+        initNetlifyCms(window.CMS_CUSTOM_CONFIG, {
+          baseDir: window.PKG_BASE_DIR,
+          state
+        }).then(config => {
+          console.log('Netlify CMS config:', config)
+        })
+      })
   })
