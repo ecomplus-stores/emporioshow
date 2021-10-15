8e56fd2c0d512046664d07fa5d19ae5cdcece2db
Leonardo Matos <leomp120894@gmail.com>
2021-10-15 13:44:34 -0300

    Update admin.js
    [skip ci]

--- template/js/admin.js
+++ template/js/admin.js
@@ -1,1 +1,28 @@
-import '#template/js/admin'
+import '@ecomplus/storefront-template/template/js/lib/config'
+import EcomRouter from '@ecomplus/storefront-router'
+import initNetlifyCms from '@ecomplus/storefront-template/template/js/netlify-cms/init'
+
+document.title = `Admin ~ ${document.title}`
+
+const state = {}
+
+new EcomRouter().list()
+  .then(routes => {
+    state.routes = routes
+  })
+  .catch(err => {
+    console.error(err)
+    state.routes = []
+  })
+
+  .finally(() => {
+    if (window.PKG_BASE_DIR === undefined) {
+      window.PKG_BASE_DIR = ''
+    }
+    initNetlifyCms(window.CMS_CUSTOM_CONFIG, {
+      baseDir: window.PKG_BASE_DIR,
+      state
+    }).then(config => {
+      console.log('Netlify CMS config:', config)
+    })
+  })
